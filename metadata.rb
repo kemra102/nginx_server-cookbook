a2c31efa9e9a768159a1f29dc7778d6e3751c272
Danny Roberts <danny@thefallenphoenix.net>
2016-01-26 23:21:25 +0000

    Update metadata so it posts to supermarket with extended metadata properly

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,15 @@ description 'Installs/Configures nginx.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
+%w(centos oracle redhat scientific).each do |os|
+  supports os, '>= 6.0'
+end
+
+source_url 'https://github.com/kemra102/nginx_server-cookbook' if
+  respond_to?(:source_url)
+issues_url 'https://github.com/kemra102/nginx_server-cookbook/issues' if
+  respond_to?(:issues_url)
+
 depends 'yum-epel', '>= 0.3.6'
 depends 'yum-nginx'
 depends 'zap', '>= 0.6.0'
