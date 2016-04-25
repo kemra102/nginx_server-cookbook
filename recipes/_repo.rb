137da2900a828d90371749ad50c08044fba2b5b8
Angelo San Ramon <angelo@angelosanramon.com>
2016-04-24 18:50:02 -0700

    added support for Ubuntu distro.

--- recipes/_repo.rb
+++ recipes/_repo.rb
@@ -1,12 +1,26 @@
+unless node.platform_family == 'rhel' || node.platform_family == 'debian'
+  Chef::Application.fatal!("Unsupported Linux distribution - #{node.platform}", 1)
+end
+
 case node['nginx_server']['repo']
-when 'epel'
+when 'epel' && node.platform_family?('rhel')
   include_recipe 'yum-epel::default'
 when 'nginx-stable'
-  include_recipe 'yum-nginx::default'
+  if node.platform_family?('rhel')
+    include_recipe 'yum-nginx::default'
+  else
+    include_recipe 'apt-nginx::default'
+  end
 when 'nginx-mainline'
-  node.default['yum-nginx']['repos']['nginx-stable']['managed'] = false
-  node.default['yum-nginx']['repos']['nginx-mainline']['managed'] = true
-  include_recipe 'yum-nginx::default'
+  if node.platform_family?('rhel')
+    node.default['yum-nginx']['repos']['nginx-stable']['managed'] = false
+    node.default['yum-nginx']['repos']['nginx-mainline']['managed'] = true
+    include_recipe 'yum-nginx::default'
+  else
+    node.default['apt-nginx']['repos']['nginx-stable']['managed'] = false
+    node.default['apt-nginx']['repos']['nginx-mainline']['managed'] = true
+    include_recipe 'apt-nginx::default'
+  end
 else
   Chef::Application.fatal!("#{node['nginx_server']['repo']} is not an allowed
     value for node['nginx_server']['repo'].", 1)
